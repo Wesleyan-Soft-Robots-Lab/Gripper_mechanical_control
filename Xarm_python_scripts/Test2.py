53f17631d3b7f54adb76f3f414f14412290903bf
Christian Diaz Herrera <102235823+cdiazherrera@users.noreply.github.com>
2024-10-18 13:35:20 -0400

    Create Test2.py

---
+++ Xarm_python_scripts/Test2.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import time
+import math
+import serial
+
+from xarm.wrapper import XArmAPI
+
+
+ip = '192.168.1.232'
+
+arm = XArmAPI(ip)
+
+arm.motion_enable(enable=True)
+arm.set_mode(0)
+arm.set_state(state=0)
+
+speed = 100
+arm.move_gohome(speed=speed, wait=True)
+
+arm.set_servo_angle(angle=[-100,0,0,0,0,0], speed=speed, wait=True)
+#arm.set_servo_angle(angle=[0,0,0,0,0,0], speed=speed, wait=True)
+print(arm.get_servo_angle(), arm.get_servo_angle(is_radian=True))
+
+#arm.move_gohome(wait=True)
+
+ser = serial.Serial('COM4', 9600)
+time.sleep(2)
+
+'''
+while True:
+    line = ser.readline().decode('utf-8').strip()
+    if line:
+        try:
+            value = int(line)
+            print(value)
+            if value >= 10000:
+                pass
+            elif value >= 800:
+                arm.set_servo_angle(angle=[0,0,0,0,0,0], speed=speed, wait=True)
+                # arm.set_servo_angle(angle=[0,0,-90,0,0,0])
+                # print(arm.get_servo_angle())
+            else:
+                arm.set_servo_angle(angle=[0,0,-90,0,0,0])
+        except ValueError:
+            print("Invalid input received!")
+'''
+
+arm.disconnect()
+
